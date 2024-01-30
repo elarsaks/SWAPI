ceca1438ba6e17bb1aa9ab718af868c986e7fff5
Elar Saks <elarsaks@gmail.com>
2024-01-30 15:41:16 +0200

    Add logic to switch between "warning", "info" & "error".

--- client/src/components/Util.tsx
+++ client/src/components/Util.tsx
@@ -1,15 +1,44 @@
+import { IoMdInformationCircleOutline } from "react-icons/io";
+import { IoWarningOutline } from "react-icons/io5";
 import { MdError } from "react-icons/md";
 import React from "react";
 import styled from "styled-components";
 
-const UtilStyles = styled.div`
+interface UtilStyleProps {
+  $type: "error" | "info" | "warning";
+}
+
+const UtilStyles = styled.div<UtilStyleProps>`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   background-color: #000000b6;
-  color: #e91e63;
-  border: 1px solid #e91e63;
+  color: ${(props) => {
+    switch (props.$type) {
+      case "error":
+        return "#e91e63";
+      case "info":
+        return "#06b9fa";
+      case "warning":
+        return "#ff9800";
+      default:
+        return "#06b9fa";
+    }
+  }};
+  border: 1px solid
+    ${(props) => {
+      switch (props.$type) {
+        case "error":
+          return "#e91e63";
+        case "info":
+          return "#06b9fa";
+        case "warning":
+          return "#ff9800";
+        default:
+          return "#06b9fa";
+      }
+    }};
   padding: 20px;
   border-radius: 5px;
   margin: 5px;
@@ -24,11 +53,26 @@ interface UtilProps {
   type: "error" | "info" | "warning";
 }
 
-const Error: React.FC<UtilProps> = ({ message }) => {
+const Error: React.FC<UtilProps> = ({ message, type }) => {
+  const getIcon = () => {
+    switch (type) {
+      case "error":
+        return <MdError size="40px" />;
+      case "warning":
+        return <IoWarningOutline size="40px" />;
+      case "info":
+        return <IoMdInformationCircleOutline size="40px" />;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <UtilStyles>
-      <MdError size="20px" />
-      <span>Error: {message}</span>
+    <UtilStyles $type={type}>
+      {getIcon()}
+      <span>
+        {type.toUpperCase()}: {message}
+      </span>
     </UtilStyles>
   );
 };
