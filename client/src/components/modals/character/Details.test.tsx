1c92fc80a5af3b6fe6ffad9f2e5bb8cf68060a40
Elar Saks <elarsaks@gmail.com>
2024-02-02 11:46:02 +0200

    Add debug.

---
+++ client/src/components/modals/character/Details.test.tsx
@@ -0,0 +1,13 @@
+/** // TODO: Write these tests after fixing this magical bug from details.
+//* Contextual Note:
+ * Details is a recursional compoment.
+ * And it has a weird bug in it.
+ */
+
+import React from "react";
+
+describe("Dummy Test Suite", () => {
+  it("passes automatically", () => {
+    expect(true).toBeTruthy();
+  });
+});
