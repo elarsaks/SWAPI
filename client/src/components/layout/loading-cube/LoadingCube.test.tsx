5b54a52ac78a29e9c55b677be8140723ad9f6a6f
Elar Saks <elarsaks@gmail.com>
2024-02-02 10:58:39 +0200

    Add debug.

---
+++ client/src/components/layout/loading-cube/LoadingCube.test.tsx
@@ -0,0 +1,11 @@
+/**
+//* Contextual Note:
+ * Adding 3D lib to this project was already nonsense
+ * Testing it would be even bigger nonsense
+ */
+
+describe("Dummy Test Suite", () => {
+  it("passes automatically", () => {
+    expect(true).toBeTruthy();
+  });
+});
